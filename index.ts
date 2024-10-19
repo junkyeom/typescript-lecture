02497391cf7c985ce38639416b7baa6712d8dbd1
junkyeom <junkyeom0203@naver.com>
2024-10-19 11:56:03 +0900

    type 변수에 담아쓰기 & readonly

--- index.ts
+++ index.ts
@@ -139,56 +139,103 @@
 
 /** narrowing / assertion */
 
-function 내함수(x: number | string) {
-    if (typeof x === 'string') {
-        return x+'1'
-    } else {
-        return x+1
-    }  
-}
-내함수(123)
+// function 내함수(x: number | string) {
+//     if (typeof x === 'string') {
+//         return x+'1'
+//     } else {
+//         return x+1
+//     }  
+// }
+// 내함수(123)
 
-function 내함수2(x: number | string) {
-    let array :number[] = [];
-    if (typeof x === 'number') {
-        array[0] = x;
-    } else {
-        return '문자 안돼요'
-    }
-}
-내함수2(123)
+// function 내함수2(x: number | string) {
+//     let array :number[] = [];
+//     if (typeof x === 'number') {
+//         array[0] = x;
+//     } else {
+//         return '문자 안돼요'
+//     }
+// }
+// 내함수2(123)
 
-function 내함수3(x: number | string) {
-    let array :number[] = [];
-    array[0] = x as number;
-}
-내함수2(123)
+// function 내함수3(x: number | string) {
+//     let array :number[] = [];
+//     array[0] = x as number;
+// }
+// 내함수2(123)
 
-function 클리닝함수(x : (number | string)[]) {
-    let arr:(number | string)[] = [];
-    x.forEach((a)=>{
-        if (typeof a === 'string') {
-            arr.push(parseInt(a))
-        } else {
-            arr.push(a)
-        }
-    })
-    console.log(arr)
-}
+// function 클리닝함수(x : (number | string)[]) {
+//     let arr:(number | string)[] = [];
+//     x.forEach((a)=>{
+//         if (typeof a === 'string') {
+//             arr.push(parseInt(a))
+//         } else {
+//             arr.push(a)
+//         }
+//     })
+//     console.log(arr)
+// }
 
 // 클리닝함수(['1',2,'3'])
 
-function 만들함수(x:{ subject : string|string[] }):string {
-    if(typeof x.subject === 'string') {
-        return x.subject;
-    } else if(Array.isArray(x.subject) === true) {
-        return x.subject[x.subject.length -1];
-    } else {
-        return '없음'
-    }
+// function 만들함수(x:{ subject : string|string[] }):string {
+//     if(typeof x.subject === 'string') {
+//         return x.subject;
+//     } else if(Array.isArray(x.subject) === true) {
+//         return x.subject[x.subject.length -1];
+//     } else {
+//         return '없음'
+//     }
 
+// }
+
+// console.log(만들함수( { subject : 'math' } ))  //이 경우 'math'를 return
+// console.log(만들함수( { subject : ['science', 'art', 'korean'] } )) //이 경우 'korean'을 return
+// console.log(만들함수( { hello : 'hi' } ))  //이 경우 타입에러 나면 됩니다 
+
+
+/** type 변수에 담아쓰기 & readonly */
+
+type Animal = string | number | undefined;
+let 동물 : Animal = 123;
+
+type AnimalType = { name : string, age : number};
+let 동물2 : AnimalType = { name : 'kim', age : 4}
+
+// const 변수 특징 -> object 자료 수정 가능
+const 출생지역 = { region : 'Seoul'}
+출생지역.region = 'Busan'
+
+type Girlfriend = { readonly name : string };
+const 여친:Girlfriend = { name : 'amber' }
+// 여친.name = '유라' // readonly 써서 에러남
+
+type Name = string;
+type Age = number;
+type Person = Name | Age;
+
+type PositionX = { x : number };  // type 변수 재정의 불가능
+type PositionY = { y : number };
+type NewType = PositionX & PositionY;  // & 연산자로 object 타입 extend 하기
+let position:NewType = { x:10, y:20 }
+
+// 숙제
+// -> object 타입을 정의한 type alias 두개를 & 기호로 합칠 때 중복된 속성이 있으면
+type 숙제obj1 = {name:string, age:number}
+type 숙제obj2 = {name:string, married:boolean}
+type 숙제obj = 숙제obj1 & 숙제obj2
+let 숙제오브제:숙제obj = {name : 'ㅇ', age : 29, married : true}
+
+type 숙제2 = {
+    color? : string,
+    size : number,
+    readonly position : number[]
 }
 
-console.log(만들함수( { subject : 'math' } ))  //이 경우 'math'를 return
-console.log(만들함수( { subject : ['science', 'art', 'korean'] } )) //이 경우 'korean'을 return
-console.log(만들함수( { hello : 'hi' } ))  //이 경우 타입에러 나면 됩니다 
+type 숙제3 = { 
+    name : string, phone : number, email : string 
+}
+let 인포:숙제3 = { name : 'kim', phone : 123, email : 'abc@naver.com' }
+
+type 숙제4 = { adult: boolean }
+type 숙제34 = 숙제3 & 숙제4
